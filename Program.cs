3348095cad035629da58a8a6176cf3980d10b757
INFOPULSE\Illia.Linkov <illia.linkov@infopulse.com>
2023-03-15 15:28:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static int LargestOf3(int a, int b, int c)
+{
+    int result;
+    if (b > c)
+    {
+        if (a > b)
+        { result = a; }
+        else { result = b; }
+    }
+    else
+    {
+        if (a > c)
+        { result = a; }
+        else { result = c; }
+    }
+    return result;
+}
